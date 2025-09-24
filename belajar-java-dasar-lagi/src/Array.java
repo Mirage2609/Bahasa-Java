e41fc40f0a4a04d3881ee9f0d453a903356f1c0c
Azka Putra Wirayudha <azkakuputra@gmail.com>
2025-09-24 21:49:40 +0700

    Baru sampai operasi matematika

--- belajar-java-dasar-lagi/src/Array.java
+++ belajar-java-dasar-lagi/src/Array.java
@@ -21,14 +21,37 @@ public class Array {
                 "Azka", "Putra", "Wirayudha"
         };
 
+        namaNama [1] = null;
+
+        System.out.println(namaNama[1]);
+
         int[] iniInt = new int[]{
                 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
         };
+
+        iniInt[3] = 50;
 
         long[] arrayLong = {
                 10L, 20L, 30L
         };
 
+        arrayLong[0] = 0;
+
+        System.out.println(arrayLong[0]);
+        System.out.println(iniInt[3]);
         System.out.println(arrayLong.length);
+
+
+        // Cara membuat Array didalam Array
+
+        String [][] members = {
+                {"Azka", "Putra"},
+                {"Wirayudha", "Putra"},
+                {"Putra", "Azka"}
+        };
+
+        System.out.println(members[0][1]);
+        System.out.println(members[1][0]);
+        System.out.println(members[2][1]);
     }
 }
---
+++ belajar-java-dasar-lagi/src/OperasiMatematika.java
@@ -0,0 +1,14 @@
+public class OperasiMatematika {
+    public static void main(String[] args) {
+
+        int a = 200;
+        int b = 30;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+
+}
